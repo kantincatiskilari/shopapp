9373601a527f0a75acfb5d72b154c68b07e354f2
kantincatiskilari <fatihtassoker@gmail.com>
2023-05-03 03:18:12 +0300

    first commit

--- server.js
+++ server.js
@@ -33,4 +33,4 @@ app.use((err,req,res,next) => {
     });
 });
 
-app.listen(process.env.PORT,() => console.log(`Server running on port: ${process.env.PORT}`));
+app.listen(process.env.PORT || 8800,() => console.log(`Server running on port: ${process.env.PORT || 8800}`));
